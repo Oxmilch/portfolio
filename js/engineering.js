8d059dec816de9ef8243ed7bed5688c63714b487
oxmilch <weisst12f6takex5bwtagtraumsd9@hotmail.co.jp>
2026-01-17 04:51:44 +0900

    スキルセットの一覧データの追加と読出し用関数の試作（動作しない）

--- js/engineering.js
+++ js/engineering.js
@@ -1,9 +1,61 @@
+/**
+ * エレメント作成
+ */
+class createElements{
+  /**
+   * スキルタグを作成
+   * @param {HTMLElement} target
+   */
+  skillTags(targetElement){
+    const skillRaw = [];
+    /* Jsonファイルの読み取り */
+    fetch("./doc/engineering-skill.json")
+     .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTPエラー: ${response.status}`);
+        }
+        return response.data();
+    }).then(data => {
+      skillRaw.concat(data);
+    }).catch(error => {
+      console.error('ファイル読み込み失敗:', error);
+      return;
+    });
+
+    /*  */
+    const skillArray = [];
+    for(i = 0; i < skillRaw.length; i++){
+      const skillJson = skillRaw[i].json();
+      // 初回作成
+      if(skillArray.length === 0){
+        skillArray.add({type: skillJson.type, data: []});
+      }
+
+      const matchSkill = skillArray.filter((skill) => skill.key == skillJson.type);
+      if(matchSkill){
+        //  typeが一致する場合は追加
+        matchSkill.data.add({key: skillJson.key, value: skillJson.value, order: skillJson.order});
+      } else {
+        // typeが一致しない場合は新規作成
+        skillArray.add({type: skillJson.type, data: [{key: skillJson.key, value: skillJson.value, order: skillJson.order}]});
+      }
+    }
+
+  }
+}
+
+
+/**
+ * HTML Loaded
+ */
 document.addEventListener('DOMContentLoaded', () => {
   const skillTags = document.querySelectorAll('.skill-tag');
   const projects = document.querySelectorAll('.project-card');
   const introArea = document.querySelector('.intro-area');
 
-  // 全スキルを初期状態（アクティブ）にする関数
+  /**
+   * 全スキルを初期状態（アクティブ）にする関数
+   */
   const resetSkills = () => {
     skillTags.forEach(tag => {
       tag.classList.remove('inactive');
@@ -11,7 +63,9 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   };
 
-  // 特定のスキルだけをハイライトする関数
+  /**
+   * 特定のスキルだけをハイライトする関数
+  */
   const highlightSkills = (techArray) => {
     skillTags.forEach(tag => {
       const techName = tag.dataset.tech;
@@ -26,10 +80,11 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   };
 
-  // オブザーバーの設定
-  // rootMargin: '-45% 0px -45% 0px' とすることで、
-  // 「画面の中央10%のライン」を通過したものを検知エリアにします。
-  // これにより、画面内に複数入っても「中央にあるもの」を判定しやすくなります。
+  /**
+   * オブザーバーの設定
+   * rootMargin: '-45% 0px -45% 0px' とすることで、「画面の中央10%のライン」を通過したものを検知エリアにします。
+   * これにより、画面内に複数入っても「中央にあるもの」を判定しやすくなります。
+   */
   const observerOptions = {
     root: null,
     rootMargin: '-45% 0px -45% 0px', 
@@ -63,4 +118,6 @@ document.addEventListener('DOMContentLoaded', () => {
   
   // 初期ロード時はリセット状態にしておく
   resetSkills();
+
+  // new createElements().skillTags();
 });
